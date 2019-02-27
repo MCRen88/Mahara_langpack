2c1879ed3658449863a90c57c9f268305063049e
MCRen88 <zdorovetc.alexey@gmail.com>
2019-02-27 15:26:52 +0200

    v 1.2

--- export/leap/lang/uk.utf8/export.leap.php
+++ export/leap/lang/uk.utf8/export.leap.php
@@ -13,4 +13,4 @@ defined('INTERNAL') || die();
 
 $string ['title'] = 'Leap2A';
 // TODO: посилання Leap2A стандартного формату на грунтовний документ на нашому wiki про Leap2A
-$string ['description1'] = 'Це дає вам експорт у <a href="https://web.archive.org/web/20140912151415/http://wiki.leapspecs.org/2A/specification"> Стандартний формат Leap2A </a>. Пізніше ви можете скористатися цим для імпортування даних у <a href="https://wiki.mahara.org/wiki/Developer_Area/Import//Export/Interoperability"> інші сумісні з Leap2A системи </a>, хоча експорт люди важко читати. ';
+$string ['description1'] = 'Це дає вам експорт у <a href="https://web.archive.org/web/20140912151415/http://wiki.leapspecs.org/2A/specification"> Стандартний формат Leap2A </a>. Пізніше ви можете скористатися цим для імпортування даних у <a href="https://wiki.mahara.org/wiki/Developer_Area/Import//Export/Interoperability"> інші сумісні з Leap2A системи </a> ';
